3120d73fd4003e1f8f6ff61d9d13f836471676b6
jpromanonet <programming@jpromano.net>
2021-09-06 20:06:45 -0300

    Router

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,3 +4,28 @@ const route = express.Router()
 const services = require('../services/render');
 const controller = require('../controller/controller');
 
+/**
+ *  @description Root Route
+ *  @method GET /
+ */
+route.get('/', services.homeRoutes)
+
+/**
+ *  @description add users
+ *  @method GET /add-users
+ */
+route.get('/add-user', services.add_user)
+
+/**
+ *  @description for user update
+ *  @method GET /update-user
+ */
+route.get('/update-user', services.update_user)
+
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
+module.export = route
