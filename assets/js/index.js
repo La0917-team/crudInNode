06614349fb48cc657dfbdd28dc72ad554a03ae92
jpromanonet <programming@jpromano.net>
2021-09-08 20:46:11 -0300

    Index JS entry point

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    // Declaramos dos arrays, uno no-indexado y otro que contenga la data
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.
+})
