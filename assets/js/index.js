e3b69731b1a0fd33f8eaeac85762aed054b6bcde
jpromanonet <programming@jpromano.net>
2021-09-08 21:14:11 -0300

    Index JS entry point

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,3 +26,21 @@ $("#add_user").submit(function(event){
     alert("El usuario fue agregado correctamente");
 })
 
+if(window.location.pathname == '/') {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("¿Estas seguro de borrar este usuario?")){
+            $.ajax(request).done(function(response)){
+                alert("El usuario ha sido eliminado con exito");
+                location.reload();
+            }
+        }
+    })
+}
